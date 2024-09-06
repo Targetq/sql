f0bd2069869c3a953dfb54e91c0006f57f7063cb
targetq <deniss.fibigs@gmail.com>
2024-09-06 07:27:05 +0200

    select with logical operators

---
+++ select/4..sql
@@ -0,0 +1,17 @@
+-- atlasit visus klientus, kuri dzimusi
+-- no 1990. gada un kruriem ir vairak ka 100 piunktu 
+
+use sql_store;
+-- 1.uzd 
+-- select * from customers
+-- where year (birth_date) >=1990 and (points) >=1000;
+
+-- 2.uzd 
+-- select * from customers
+-- where year (birth_date) >=1990 or (points) >=1000;
+
+-- 3.uzd
+select * from customers
+where year (birth_date) >=1990 or (points) >=1000 and state in ('VA', "CO");
+	
+
